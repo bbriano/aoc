cfa61737d7f5d1d32a7d5baee3728f4bdad9b494
Briano Goestiawan <b@briano.io>
2024-11-03 10:40:24 +1100

    2015/03: simplify

--- 2015/03/sol.go
+++ 2015/03/sol.go
@@ -7,51 +7,32 @@ import (
 
 func main() {
 	input, _ := os.ReadFile("input")
-	part1 := make(map[[2]int]int)
-	part2 := make(map[[2]int]int)
+	seen := make(map[[2]int]bool)
+	seen2 := make(map[[2]int]bool)
 	var santa, santa2, robot [2]int
-	part1[santa]++
-	part2[santa2]++
-	part2[robot]++
+	seen[santa], seen2[santa2] = true, true
 	for i, c := range input {
+		who := &santa2
+		if i%2 == 1 {
+			who = &robot
+		}
 		switch c {
-		case '<':
-			santa[1]--
-		case '>':
-			santa[1]++
 		case '^':
 			santa[0]--
+			(*who)[0]--
 		case 'v':
 			santa[0]++
+			(*who)[0]++
+		case '<':
+			santa[1]--
+			(*who)[1]--
+		case '>':
+			santa[1]++
+			(*who)[1]++
 		}
-		part1[santa]++
-		switch i % 2 {
-		case 0:
-			switch c {
-			case '<':
-				santa2[1]--
-			case '>':
-				santa2[1]++
-			case '^':
-				santa2[0]--
-			case 'v':
-				santa2[0]++
-			}
-			part2[santa2]++
-		case 1:
-			switch c {
-			case '<':
-				robot[1]--
-			case '>':
-				robot[1]++
-			case '^':
-				robot[0]--
-			case 'v':
-				robot[0]++
-			}
-			part2[robot]++
-		}
+		seen[santa] = true
+		seen2[*who] = true
 	}
-	fmt.Println("part1:", len(part1))
-	fmt.Println("part2:", len(part2))
+	fmt.Println("part1:", len(seen))
+	fmt.Println("part2:", len(seen2))
 }
