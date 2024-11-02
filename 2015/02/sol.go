c390bfdcb9965e56fb216fe9e6c6dbb4a50719a8
Briano Goestiawan <b@briano.io>
2024-11-03 10:11:44 +1100

    regexp is overkill

--- 2015/02/sol.go
+++ 2015/02/sol.go
@@ -3,21 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
 )
 
 func main() {
-	input, _ := os.ReadFile("input")
-	re := regexp.MustCompile(`(.+)x(.+)x(.+)`)
-	paper, ribbon := 0, 0
-	for _, match := range re.FindAllStringSubmatch(string(input), -1) {
-		l, _ := strconv.Atoi(match[1])
-		w, _ := strconv.Atoi(match[2])
-		h, _ := strconv.Atoi(match[3])
-		a, b, c := l*w, w*h, h*l
-		paper += 2*(a+b+c) + min(a, b, c)
-		ribbon += 2*min(l+w, w+h, h+l) + l*w*h
+	f, _ := os.Open("input")
+	defer f.Close()
+	var paper, ribbon int
+	for {
+		var x, y, z int
+		if _, err := fmt.Fscanf(f, "%dx%dx%d\n", &x, &y, &z); err != nil {
+			break
+		}
+		paper += 2*(x*y+y*z+z*x) + x*y*z/max(x, y, z)
+		ribbon += 2*(x+y+z-max(x, y, z)) + x*y*z
 	}
 	fmt.Println("part1:", paper)
 	fmt.Println("part2:", ribbon)
