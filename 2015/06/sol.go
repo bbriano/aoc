08e9129f5a3dd67004fcf7ed0dab4669a2e59f7f
Briano Goestiawan <b@briano.io>
2024-11-03 12:31:08 +1100

    2015/06: update

--- 2015/06/sol.go
+++ 2015/06/sol.go
@@ -2,68 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
+	"regexp"
 	"strconv"
-	"strings"
 )
 
 func main() {
-	f, err := os.Open("input")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	buf, err := io.ReadAll(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	lines := strings.Split(string(buf), "\n")
-	light1 := [1000][1000]bool{}
-	light2 := [1000][1000]int{}
-	for _, line := range lines {
-		idx := strings.IndexAny(line, "0123456789")
-		cmd := line[:idx-1]
-		f := strings.Split(line[idx:], " ")
-		p1 := strings.Split(f[0], ",")
-		p2 := strings.Split(f[2], ",")
-		p1x, p1y := atoi(p1[0]), atoi(p1[1])
-		p2x, p2y := atoi(p2[0]), atoi(p2[1])
-		for y := p1y; y <= p2y; y++ {
-			for x := p1x; x <= p2x; x++ {
-				switch cmd {
-				case "turn on":
-					light1[y][x] = true
-					light2[y][x]++
-				case "toggle":
-					light1[y][x] = !light1[y][x]
-					light2[y][x] += 2
+	input, _ := os.ReadFile("input")
+	const N = 1000
+	var grid [N][N]bool
+	var grid2 [N][N]int
+	re := regexp.MustCompile(`(.*) (\d+),(\d+) through (\d+),(\d+)`)
+	for _, match := range re.FindAllStringSubmatch(string(input), -1) {
+		x0, y0 := atoi(match[2]), atoi(match[3])
+		x1, y1 := atoi(match[4]), atoi(match[5])
+		for y := y0; y <= y1; y++ {
+			for x := x0; x <= x1; x++ {
+				switch match[1] {
 				case "turn off":
-					light1[y][x] = false
-					light2[y][x]--
-					if light2[y][x] < 0 {
-						light2[y][x] = 0
-					}
-				default:
-					panic("unhandled cmd")
+					grid[y][x] = false
+					grid2[y][x] = max(0, grid2[y][x]-1)
+				case "turn on":
+					grid[y][x] = true
+					grid2[y][x]++
+				case "toggle":
+					grid[y][x] = !grid[y][x]
+					grid2[y][x] += 2
 				}
 			}
 		}
 	}
-
-	part1, part2 := 0, 0
-	for y := range light1 {
-		for x := range light1[y] {
-			if light1[y][x] {
-				part1++
+	var c, c2 int
+	for y := range N {
+		for x := range N {
+			if grid[y][x] {
+				c++
 			}
-			part2 += light2[y][x]
+			c2 += grid2[y][x]
 		}
 	}
-	fmt.Println("part1:", part1)
-	fmt.Println("part2:", part2)
+	fmt.Println("part1:", c)
+	fmt.Println("part2:", c2)
 }
 
 func atoi(s string) int {
