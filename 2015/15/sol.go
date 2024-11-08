cad00423c7c4d05dd32a3e53e1056fecec3929e5
Briano Goestiawan <b@briano.io>
2024-11-09 05:16:31 +1100

    2015/15

--- 2015/15/sol.go
+++ 2015/15/sol.go
@@ -2,77 +2,61 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	f, err := os.Open("input")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	buf, err := io.ReadAll(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	var ingredients [][]int
-	for _, line := range strings.Split(string(buf), "\n") {
-		f := strings.Split(line, ": ")
-		var ing []int
-		for _, prop := range strings.Split(f[1], ", ") {
-			f := strings.Split(prop, " ")
-			val, err := strconv.Atoi(f[1])
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
-			ing = append(ing, val)
+	input, _ := os.ReadFile("input")
+	var ingredient [][]int
+	for _, line := range strings.Split(string(input), "\n") {
+		line = strings.Split(line, ":")[1]
+		var prop []int
+		for _, clause := range strings.Split(line, ",") {
+			f := strings.Fields(clause)
+			n, _ := strconv.Atoi(f[1])
+			prop = append(prop, n)
 		}
-		ingredients = append(ingredients, ing)
+		ingredient = append(ingredient, prop)
 	}
-	part1, part2 := 0, 0
-	for _, teaspoon := range share(100, 5) {
-		props := make([]int, len(ingredients[0]))
-		for i := range ingredients {
-			for j := 0; j < len(ingredients[i]); j++ {
-				props[j] += ingredients[i][j] * teaspoon[i]
+	maxscore, maxscore2 := math.MinInt, math.MinInt
+	for _, teaspoon := range partition(100, len(ingredient)) {
+		prop := make([]int, len(ingredient[0]))
+		for i := range prop {
+			for j := range ingredient {
+				prop[i] += teaspoon[j] * ingredient[j][i]
 			}
 		}
-		score := 1
-		for i := 0; i < len(props)-1; i++ {
-			if props[i] < 0 {
-				score = 0
-			}
-			score *= props[i]
-		}
-		if score > part1 {
-			part1 = score
-		}
-		if props[4] == 500 && score > part2 {
-			part2 = score
+		maxscore = max(maxscore, score(prop))
+		if prop[4] == 500 {
+			maxscore2 = max(maxscore2, score(prop))
 		}
 	}
-	fmt.Println("part1:", part1)
-	fmt.Println("part2:", part2)
+	fmt.Println("part1:", maxscore)
+	fmt.Println("part2:", maxscore2)
 }
 
-func share(n, p int) [][]int {
-	var out [][]int
-	if p == 1 {
+func partition(n, k int) [][]int {
+	if k == 1 {
 		return [][]int{{n}}
 	}
-	for i := 0; i <= n; i++ {
-		for _, sub := range share(n-i, p-1) {
-			el := []int{i}
-			for _, s := range sub {
-				el = append(el, s)
-			}
-			out = append(out, el)
+	var out [][]int
+	for i := range n + 1 {
+		for _, sub := range partition(n-i, k-1) {
+			s := []int{i}
+			s = append(s, sub...)
+			out = append(out, s)
 		}
+	}
+	return out
+}
+
+func score(prop []int) int {
+	out := 1
+	for _, p := range prop[:4] {
+		out *= max(0, p)
 	}
 	return out
 }
