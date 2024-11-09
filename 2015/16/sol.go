351e46483d89e7fc32d17f042b46ad939bd405a1
Briano Goestiawan <b@briano.io>
2024-11-10 01:32:12 +1100

    2015/16

--- 2015/16/sol.go
+++ 2015/16/sol.go
@@ -1,65 +1,47 @@
 package main
 
 import (
-	"strings"
 	"fmt"
 	"os"
 	"strconv"
-	"io"
+	"strings"
 )
 
+var gift = map[string]int{
+	"children":    3,
+	"cats":        7,
+	"samoyeds":    2,
+	"pomeranians": 3,
+	"akitas":      0,
+	"vizslas":     0,
+	"goldfish":    5,
+	"trees":       3,
+	"cars":        2,
+	"perfumes":    1,
+}
+
 func main() {
-	f, err := os.Open("input")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	buf, err := io.ReadAll(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	target := map[string]int{
-		"children": 3,
-		"cats": 7,
-		"samoyeds": 2,
-		"pomeranians": 3,
-		"akitas": 0,
-		"vizslas": 0,
-		"goldfish": 5,
-		"trees": 3,
-		"cars": 2,
-		"perfumes": 1,
-	}
-	for _, line := range strings.Split(string(buf), "\n") {
-		sue, props, ok := strings.Cut(line, ": ")
-		if !ok {
-			panic("bad cut")
-		}
+	input, _ := os.ReadFile("input")
+	for _, line := range strings.Split(string(input), "\n") {
 		part1, part2 := true, true
-		for _, prop := range strings.Split(props, ", ") {
-			key, val, ok := strings.Cut(prop, ": ")
-			if !ok {
-				panic("bad cut")
-			}
-			n, err := strconv.Atoi(val)
-			if err != nil {
-				panic(err)
-			}
-			if n != target[key] {
+		sue, line, _ := strings.Cut(line, ": ")
+		for _, props := range strings.Split(line, ", ") {
+			key, v, _ := strings.Cut(props, ": ")
+			value, _ := strconv.Atoi(v)
+			if value != gift[key] {
 				part1 = false
 			}
 			switch key {
 			case "cats", "trees":
-				if n <= target[key] {
+				if value <= gift[key] {
 					part2 = false
 				}
 			case "pomeranians", "goldfish":
-				if n >= target[key] {
+				if value >= gift[key] {
 					part2 = false
 				}
 			default:
-				if n != target[key] {
+				if value != gift[key] {
 					part2 = false
 				}
 			}
