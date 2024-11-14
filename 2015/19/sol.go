fad6d4cfcd16cb7f9127dfcb9c5e308ebb690fd0
Briano Goestiawan <b@briano.io>
2024-11-15 07:40:16 +1100

    2015/19: still not satisfied with part 2

--- 2015/19/sol.go
+++ 2015/19/sol.go
@@ -2,53 +2,35 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	f, err := os.Open("input")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	input, _ := os.ReadFile("input")
+	var repls [][2]string
+	replacements, medicine, _ := strings.Cut(string(input), "\n\n")
+	for _, line := range strings.Split(replacements, "\n") {
+		from, to, _ := strings.Cut(line, " => ")
+		repls = append(repls, [2]string{from, to})
 	}
-	defer f.Close()
-	buf, err := io.ReadAll(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	lines := strings.Split(string(buf), "\n")
-	transform := make(map[string][]string)
-	for _, line := range lines[:len(lines)-2] {
-		f := strings.Split(line, " => ")
-		from, to := f[0], f[1]
-		transform[from] = append(transform[from], to)
-	}
-	medicine := lines[len(lines)-1]
-
-	count := make(map[string]struct{})
-	for from, tos := range transform {
-		for i := 0; i <= len(medicine)-len(from); i++ {
-			if medicine[i:i+len(from)] == from {
-				for _, to := range tos {
-					str := medicine[:i] + to + medicine[i+len(from):]
-					count[str] = struct{}{}
-				}
+	next := make(map[string]bool)
+	for i := range medicine {
+		for _, rep := range repls {
+			from, to := rep[0], rep[1]
+			if strings.HasPrefix(medicine[i:], from) {
+				next[medicine[:i]+to+medicine[i+len(from):]] = true
 			}
 		}
 	}
-	fmt.Println("part1:", len(count))
-
+	fmt.Println("part1:", len(next))
 	steps := 0
 	for medicine != "e" {
-		for from, tos := range transform {
-			for _, to := range tos {
-				if strings.Contains(medicine, to) {
-					medicine = strings.Replace(medicine, to, from, 1)
-					steps++
-				}
+		for _, rep := range repls {
+			from, to := rep[0], rep[1]
+			if strings.Contains(medicine, to) {
+				medicine = strings.Replace(medicine, to, from, 1)
+				steps++
 			}
 		}
 	}
