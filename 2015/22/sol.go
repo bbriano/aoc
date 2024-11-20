6fcd4874ffc4dd58fbf7bebdcc4887811b396000
Briano Goestiawan <b@briano.io>
2024-11-21 08:04:46 +1100

    2015/22: part1 cleanup

--- 2015/22/sol.go
+++ 2015/22/sol.go
@@ -2,10 +2,47 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type player struct{ hp, mana, armor int }
 type boss struct{ hp, damage int }
+
+type spell struct {
+	mana    int
+	instant func(*player, *boss)
+	effect  effect
+}
+
+type effect struct {
+	timer int
+	f     func(*player, *boss, int)
+}
+
+type effectMap map[int]*effect
+
+func (effects effectMap) apply(p *player, b *boss) {
+	for i, eff := range effects {
+		if eff.timer <= 0 {
+			delete(effects, i)
+			continue
+		}
+		eff.timer--
+		eff.f(p, b, eff.timer)
+		if eff.timer <= 0 {
+			delete(effects, i)
+		}
+	}
+}
+
+func (effects effectMap) clone() effectMap {
+	out := make(effectMap)
+	for i, eff := range effects {
+		effCopy := *eff
+		out[i] = &effCopy
+	}
+	return out
+}
 
 var spells = []spell{
 	{53, func(p *player, b *boss) { b.hp -= 4 }, effect{}},
@@ -19,131 +56,44 @@ var spells = []spell{
 	{229, func(*player, *boss) {}, effect{5, func(p *player, b *boss, timer int) { p.mana += 101 }}},
 }
 
-var spellNames = []string{"Missile", "Drain", "Shield", "Poison", "Recharge"}
-
-type spell struct {
-	mana    int
-	instant instant
-	effect  effect
-}
-
-type instant func(*player, *boss)
-
-type effect struct {
-	timer int
-	f     func(*player, *boss, int)
-}
-
-// return value reports if the effect should still be active.
-func (e *effect) apply(p *player, b *boss) bool {
-	if e.timer <= 0 {
-		return false
-	}
-	e.timer--
-	e.f(p, b, e.timer)
-	return e.timer > 0
-}
-
 func main() {
 	p := player{50, 500, 0}
 	b := boss{71, 10}
-	//p = player{10, 250, 0}
-	//b = boss{13, 8}
-	minimize(p, b, make(map[int]*effect), 0, nil)
-	fmt.Println("Done\n")
+	minimize(p, b, make(effectMap), 0)
 	fmt.Println("part1:", mincost)
-	fmt.Println(best)
-	effects := make(map[int]*effect)
-	for _, i := range best {
-		sp := spells[i]
-
-		fmt.Println("Player")
-		fmt.Println(p, b)
-		for i, eff := range effects {
-			fmt.Println("Effect", spellNames[i])
-			if !eff.apply(&p, &b) {
-				delete(effects, i)
-			}
-		}
-		fmt.Println(p, b)
-		fmt.Println("Cast", spellNames[i])
-		p.mana -= sp.mana
-		sp.instant(&p, &b)
-		effects[i] = &sp.effect
-		fmt.Println(p, b)
-		fmt.Println()
-
-		fmt.Println("Boss")
-		fmt.Println(p, b)
-		for i, eff := range effects {
-			fmt.Println("Effect", spellNames[i])
-			if !eff.apply(&p, &b) {
-				delete(effects, i)
-			}
-		}
-		fmt.Println(p, b)
-		p.hp -= max(1, b.damage-p.armor)
-		fmt.Println(p, b)
-		fmt.Println()
-	}
-	fmt.Println(p, b)
 }
 
-var mincost = 9999999999
+var mincost = math.MaxInt
 
-var best []int
-
-func minimize(p player, b boss, effects map[int]*effect, cost int, casted []int) {
+func minimize(p player, b boss, effects effectMap, cost int) {
 	if cost >= mincost {
 		return
 	}
-	// player's turn
-	for i, eff := range effects {
-		if !eff.apply(&p, &b) {
-			delete(effects, i)
-		}
-	}
+	// Player's turn
+	effects.apply(&p, &b)
 	for i, sp := range spells {
 		if p.mana < sp.mana {
 			continue
 		}
 		if _, ok := effects[i]; ok {
-			// can't have multiple of the same effect active
+			// Can't have multiple of the same effect active.
 			continue
 		}
-		castedCopy := make([]int, len(casted)+1)
-		copy(castedCopy, casted)
-		castedCopy[len(casted)] = i
-		p, b, effects := p, b, clone(effects)
+		// Copy state to not affect previous/next iteration.
+		p, b, effects := p, b, effects.clone()
 		p.mana -= sp.mana
 		sp.instant(&p, &b)
 		effects[i] = &sp.effect
-		// boss's turn
-		for i, eff := range effects {
-			if !eff.apply(&p, &b) {
-				delete(effects, i)
-			}
-		}
+		// Boss's turn
+		effects.apply(&p, &b)
 		if b.hp <= 0 {
-			if cost+sp.mana < mincost {
-				mincost = cost + sp.mana
-				best = castedCopy
-			}
+			mincost = min(mincost, cost+sp.mana)
 			continue
 		}
 		p.hp -= max(1, b.damage-p.armor)
 		if p.hp <= 0 {
 			continue
 		}
-		minimize(p, b, effects, cost+sp.mana, castedCopy)
+		minimize(p, b, effects, cost+sp.mana)
 	}
-}
-
-func clone(effects map[int]*effect) map[int]*effect {
-	out := make(map[int]*effect)
-	for i, eff := range effects {
-		effCopy := *eff
-		out[i] = &effCopy
-	}
-	return out
 }
